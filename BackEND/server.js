ac20305d535879a0b81120442645afa9b77b587b
Adeesh Garg <adeesh.g47@gmail.com>
2023-12-20 18:29:00 +0530

    Added home url

--- BackEND/server.js
+++ BackEND/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.redirect("https://swiftlink.onrender.com/");
 });
 
 app.get('/healthCheck', (req, res) => {
