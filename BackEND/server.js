b36176367ce2183d67117bac6078c94fa345b716
Adeesh Garg <adeesh.g47@gmail.com>
2023-12-20 18:37:15 +0530

    Home redirection link changed

--- BackEND/server.js
+++ BackEND/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.redirect("https://swiftlink.onrender.com/");
+    res.redirect("https://swiftlink-web.onrender.com");
 });
 
 app.get('/healthCheck', (req, res) => {
