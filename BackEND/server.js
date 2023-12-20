285f5c574bcc9360b4133ec48342c20dad273e74
Adeesh Garg <adeesh.g47@gmail.com>
2023-12-20 17:28:08 +0530

    Server side functionality created

---
+++ BackEND/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+const { URLs } = require('./models');
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Update with your client's origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+    const { shortened, link } = req.body;
+    URLs.create({
+        shortened,
+        link
+    });
+});
+
+app.get('/:shortened', (req, res) => {
+    const { shortened } = req.params;
+    URLs.findOne({
+        where: {
+            shortened
+        }
+    }).then((result) => {
+        const { link } = result;
+        console.log(link);
+        res.redirect(link);
+    });
+});
+
+app.listen(port);
+
+
