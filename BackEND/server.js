d09ccb5dea764e2eed48b478ec74e47575a420e6
Adeesh Garg <adeesh.g47@gmail.com>
2023-12-21 19:26:57 +0530

    Updated CORS

--- BackEND/server.js
+++ BackEND/server.js
@@ -9,7 +9,7 @@ const { URLs } = require('./models');
 app.use(express.static(path.join(__dirname, '..', 'FrontEnd', 'dist')));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Update with your client's origin
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
